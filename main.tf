b5f009e216383af6d62584ae760d621e434f0bc8
emilmathachan <107055688+emilmathachan@users.noreply.github.com>
2025-11-05 10:33:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 provider "azurerm" {
-   features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
-
 resource "azurerm_resource_group" "example" {
   name     = "example-resources5"
   location = "West Europe"
