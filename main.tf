7d96debe63517498fed5098bf00004feee734b80
emilmathachan <107055688+emilmathachan@users.noreply.github.com>
2026-06-29 11:45:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   features {}
-  subscription_id = "97749a8e-c95f-4c22-bbb3-4e3a491c25bd" # Replace with your actual Subscription ID
+  subscription_id = "cb2551fc-f8e3-4dfc-a787-cf72cf4c862e" # Replace with your actual Subscription ID
 }
 resource "azurerm_resource_group" "example" {
   name     = "example-resources5"
